a239a8fcdccd9725b0f7fe03eee02494b1650fe4
JIN QIKAI <19034275@myrp.edu.sg>
2020-05-04 14:16:51 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19034275/p02/Ex3b.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19034275.p02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex3b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_ex3b);
+    }
+}
